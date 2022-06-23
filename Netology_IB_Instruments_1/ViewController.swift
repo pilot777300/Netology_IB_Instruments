ddc43d2920038f184801234ef40858b956abc3a3
pilot777300 <anton225@mail.ru>
2022-06-23 19:53:41 +0300

    Initial Commit

---
+++ Netology_IB_Instruments_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments_1
+//
+//  Created by Mac on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
